95cbf78e74643a5643426b09271b404424efa12d
Michael228 <michaeltest228@gmail.com>
2024-12-28 11:34:14 +0300

    Global update.

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = make_pipeline(TfidfVectorizer(), MultinomialNB())
 
 model.fit(X_train, y_train)
+
 argparser = argparse.ArgumentParser()
 
 argparser.add_argument('--text', type=str, help='Text to classify')
