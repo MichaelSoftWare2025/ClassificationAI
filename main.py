431251252da4647bcdd8a5926fadfda637cf3bb8
Michael228 <michaeltest228@gmail.com>
2024-12-28 18:15:35 +0300

    Global update.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import joblib
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import make_pipeline
@@ -23,7 +23,7 @@ y = df['label']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+model = make_pipeline(CountVectorizer(), MultinomialNB())
 
 model.fit(X_train, y_train)
 
